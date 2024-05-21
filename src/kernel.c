632732e5d3830a26e55f291c14aba06bef28ff94
Bukayoooo <hz19981004@gmail.com>
2024-05-21 23:56:13 +0800

    add memcpy test

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,8 @@
 #include "uart.h"
 
+extern void load_store_test(void);
+extern void memcpy_test(void);
+
 void load_test()
 {
 	load_store_test();
@@ -12,6 +15,7 @@ void kernel_main(void)
 	uart_send_string("Welcome RISC-V!\r\n");
 
 	load_test();
+	memcpy_test();
 
 	while (1) {
 		;
